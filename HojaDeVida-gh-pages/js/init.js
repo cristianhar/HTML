f2900842a80f565f3e09b4b9f6aa51b0098383d7
cristianhar~ <yohan4752@gmail.com>
2024-02-21 08:14:02 -0500

    first commit

--- HojaDeVida-gh-pages/js/init.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-  $('.tabs').tabs();
-  $('.tooltipped').tooltip();
-  $('.modal').modal();
-  $('.datepicker').datepicker();
-  $('select').formSelect();
-});
